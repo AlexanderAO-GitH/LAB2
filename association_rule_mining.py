22d899ef2427ce0baddd0b1a0480b2df1658d817
Zeheng Li <imzehengl@gmail.com>
2023-11-16 01:41:03 -0700

    Update association_rule_mining.py

--- association_rule_mining.py
+++ association_rule_mining.py
@@ -5,71 +5,78 @@ df = pd.read_pickle("UK.pkl")
 
 df.head()
 
-type(df)
+
 # %% convert dataframe to invoice-based transactional format
-import pandas as pd
 from mlxtend.preprocessing import TransactionEncoder
-df = pd.read_pickle("UK.pkl")
+
+df_invoice = df.groupby(["InvoiceNo"]).apply(
+    lambda gdf: gdf["Description"].to_list()
+)
 
 te = TransactionEncoder()
-te_ary = te.fit(df).transform(df)
-df_f = pd.DataFrame(te_ary, columns=te.columns_)
-
+transactions = te.fit_transform(df_invoice)
+transactions = pd.DataFrame(transactions, columns=te.columns_)
+transactions
 
 
 # %% apply apriori algorithm to find frequent items and association rules
-import pandas as pd
-from mlxtend.preprocessing import TransactionEncoder
-from mlxtend.frequent_patterns import apriori
-df = pd.read_pickle("UK.pkl")
+from mlxtend.frequent_patterns import apriori, association_rules
 
-
-te = TransactionEncoder()
-te_ary = te.fit(df).transform(df)
-df_f = pd.DataFrame(te_ary, columns=te.columns_)
-appp = apriori(df_f, min_support=0.2, use_colnames=True, verbose=1)
-
-appp.head()
+freq_items = apriori(
+    transactions,
+    min_support=0.01,
+    use_colnames=True,
+    verbose=1,
+)
+rules = association_rules(
+    freq_items,
+    metric="confidence",
+    min_threshold=0.6,
+)
 
 
 # %% count of frequent itemsets that have more then 1/2/3 items,
 # and the frequent itemsets that has the most items
-import pandas as pd
-from mlxtend.preprocessing import TransactionEncoder
-from mlxtend.frequent_patterns import apriori
-df = pd.read_pickle("UK.pkl")
+freq_items["length"] = freq_items["itemsets"].apply(len)
+print((freq_items["length"] > 1).sum())
+print((freq_items["length"] > 2).sum())
+print((freq_items["length"] > 3).sum())
+print(freq_items["length"].max())
 
-te = TransactionEncoder()
-te_ary = te.fit(df).transform(df)
-df_f = pd.DataFrame(te_ary, columns=te.columns_)
-apriori(df_f, min_support=0.5, use_colnames=True)
-
-frequent_itemsets = apriori(df_f, min_support=0.6, use_colnames=True)
-frequent_itemsets['length'] = frequent_itemsets['itemsets'].apply(lambda x: len(x))
-frequent_itemsets
+print(
+    freq_items[freq_items["length"] == freq_items["length"].max()][
+        "itemsets"
+    ].tolist()
+)
 
 
 # %% top 10 lift association rules
-import pandas as pd
-import numpy as np
-from mlxtend.frequent_patterns import apriori, association_rules
-import matplotlib.pyplot as plt
 
-df = pd.read_pickle("UK.pkl", sep=',')
-df.head(10)
-frequent_itemsets = apriori(df_f, min_support=0.6, use_colnames=True)
-apriori(df, min_support=0.5, use_colnames=False, max_len=None, verbose=0, low_memory=False)
-rules = association_rules(freq_items, metric="confidence", min_threshold=0.6)
-rules.head()
+rules.sort_values("lift", ascending=False).head(10)[
+    ["antecedents", "consequents"]
+]
+
 
 # %% scatterplot support vs confidence
 import seaborn as sns
-import matplotlib.pyplot as plt
 
-sns.scatterplot(x=rules["support"], y=rules["confidence"], alpha=0.5)
-plt.xlabel("Support")
-plt.ylabel("Confidence")
-plt.title("Support vs Confidence")
+ax = sns.scatterplot(
+    data=rules,
+    x="support",
+    y="confidence",
+    alpha=0.5,
+)
+ax.set(
+    xlabel="Support",
+    ylabel="Confidence",
+    title="Support vs Confidence",
+)
 
 
 # %% scatterplot support vs lift
+ax = sns.scatterplot(data=rules, x="support", y="lift", alpha=0.5)
+ax.set(
+    xlabel="Support",
+    ylabel="Lift",
+    title="Support vs Lift",
+)
